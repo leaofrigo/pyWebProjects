544a67c514d9543c5f67d84ff992f9c616536b2a
Anderson <anderson.frigo@macsonline.de>
2021-02-04 20:50:06 +0100

    New features for templates

--- webPyAPI/webPyAPI_app/views.py
+++ webPyAPI/webPyAPI_app/views.py
@@ -8,17 +8,24 @@ def index(request):
 # # views to get API data
 def get_data_API(request):
     import requests
-    # # example URL for total population
-    url='http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json'
-    # # request and save data in a dictionary "data"
-    data=requests.get(url).json()
-    # # verify data type
-    type(data[1][2])
-    # # verify dictionary length
-    len(data[1][:])
-    # # gets all years
-    years=[]
-    for i in range(1,len(data[1][:])):
-        years.append(data[1][i]['date'])
-    print(years)
-    return render(request,'webPyAPI_app/index.html')
+
+    # url to get metadata
+    url_meta='http://api.worldbank.org/v2/sources/2/country/data?format=json'
+    meta=requests.get(url_meta).json()
+
+    # number of countries in DB
+    nr_cnt=meta['total']
+
+    # create precise url string for API
+    url_nr_cnt='http://api.worldbank.org/v2/sources/2/country/data?format=json&per_page=%s' % (nr_cnt)
+    countries=requests.get(url_nr_cnt).json()
+
+    # country ids
+    country_ids=[]
+    for i in range(0,nr_cnt):
+        c_id = countries['source'][0]['concept'][0]['variable'][i]['id']
+        c_name = countries['source'][0]['concept'][0]['variable'][i]['value']
+        country_ids.append([c_id,c_name])
+
+    context = {'country_ids':country_ids,'numb_countries':nr_cnt}
+    return render(request,'webPyAPI_app/index.html',context)
